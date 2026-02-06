ff1b98dde3fafeb8f3f51af8aeb3dc2321bb4457
Eduardo Dezena <eduardodezna@gmail.com>
2026-02-05 22:02:15 -0300

    Configure Redux store

--- web/src/store/store.ts
+++ web/src/store/store.ts
@@ -1,8 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
-  reducer: {
-  },
+    reducer: {
+        init: (state = {}) => state,
+    },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
